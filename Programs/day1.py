39c9af09ccb0096bdd1cc13c3ef4f37267dbd0e8
Achika_Tiwari <achikatiwari@gmail.com>
2024-07-31 15:36:57 +0530

    Day 1 Print() function and seperator use

---
+++ Programs/day1.py
@@ -0,0 +1,45 @@
+# First Programm : Print "Hello world"
+
+# print("Hello World")
+
+# print() function :
+# 2.
+
+# a=5
+# b=6
+# print(a+b)
+
+# 3.
+
+# a="Hello"
+# print(a)
+
+# 4.
+
+# a="Hello"
+# b="World"
+# print(a+b)
+
+# note:- print without space 
+
+# 5.
+
+# a="Hello"
+# b=5
+# print(a+b)
+
+# note:- Concatination not possible in diffrent type of data type in python
+
+# 6. calling blank print() function 
+# print()
+# note :- blank print 
+
+# 7.
+
+# print("Hello","How","are","you")
+
+# 8. saperator
+
+print("Hello","How","are","you", sep="^")
+
+
