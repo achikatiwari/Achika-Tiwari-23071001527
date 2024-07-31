cf7726f74363b4cda314ddbdc4f0890c73d6dba5
Achika_Tiwari <achikatiwari@gmail.com>
2024-07-31 16:08:07 +0530

    Update file

--- Programs/day1.py
+++ Programs/day1.py
@@ -40,10 +40,42 @@
 
 # 8. saperator
 
-print("Hello","How","are","you", end="^")
 
-# note:- using sep() fuction we can easly saperate any word in any type on sep which we want we only need to give sep in "" double codes
+print("Hello","How","are","you", sep="-")
+print("Hello","How","are","you", end="-")
+
+# 9.
+# a=5
+# b=3.16
+# print(a+b)
 
 
+# Variable :
+# a=5
+# b=6
+# c
+# print(a,b,c)
 
+# note:- Error : c can not initilise
 
+# Declaring Multiple Value :
+
+# a,b,c = 1,2,3
+# print(a,b,c)
+
+# name,price,exDate = 'soup','45', '12-10-2023'
+# print(name,price,exDate)
+
+# id() function
+
+# a=10
+# b=9
+# print(id(a))
+# print(id(b))
+
+# Same value
+
+# a=10
+# b=10
+# print(id(a))
+# print(id(b))
